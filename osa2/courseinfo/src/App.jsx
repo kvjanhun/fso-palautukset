eb4517f800e096eabe6ef82031193856521a2fbe
erezac <konsta@erez.ac>
2025-07-21 10:26:56 +0300

    2.1: Course information step 6

--- osa2/courseinfo/src/App.jsx
+++ osa2/courseinfo/src/App.jsx
@@ -2,9 +2,9 @@ const Header = (props) => <h1>{props.course}</h1>
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 )
 
@@ -18,36 +18,48 @@ const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3
       },
+      {
+        name: 'Nonsense for testing',
+        exercises: 69,
+        id: 5
+      }
     ],
   }
 
-  return (
+  const Course = () => (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
     </div>
   )
+
+  return (
+    <>
+      <Course course={course} />
+    </>
+  )
 }
+
+// Saved for later use, move to Content if needed
+//<Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+
 
 export default App
