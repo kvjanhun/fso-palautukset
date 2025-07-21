b30aa78ed9012b14ca669f3255fca7604d3cd958
erezac <konsta@erez.ac>
2025-07-21 11:13:34 +0300

    2.5: Separate module step 9

--- osa2/courseinfo/src/App.jsx
+++ osa2/courseinfo/src/App.jsx
@@ -1,13 +1,19 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Header = ({ text }) => <h1>{text}</h1>
+const SubHeader = ({ text }) => <h2>{text}</h2>
 const Content = (props) => (
   <div>
     {props.parts.map((part) => (
       <Part key={part.id} part={part} />
     ))}
-    <hr />
     <Total total={getTotalExercises(props.parts)} />
   </div>
 )
+const Course = ({ course }) => (
+    <div>
+      <SubHeader text={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
 const Part = (props) => (
   <p>
     {props.part.name} {props.part.exercises}
@@ -19,43 +25,57 @@ const getTotalExercises = (parts) =>
   parts.reduce((sum, part) => sum + part.exercises, 0)
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Nonsense for testing',
-        exercises: 69,
-        id: 5
-      }
-    ],
-  }
-
-  const Course = () => (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-    </div>
-  )
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Nonsense for testing',
+          exercises: 69,
+          id: 5
+        }
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
+      <Header text="Web development curriculum" />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </>
   )
 }
