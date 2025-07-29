e880b7e21a135d10a8f787c5bcb8404d9ac605a6
erezac <konsta@erez.ac>
2025-07-29 15:49:22 +0300

    Include instructions on form placeholders

--- osa2/phonebook/src/components/PersonForm.jsx
+++ osa2/phonebook/src/components/PersonForm.jsx
@@ -1,9 +1,15 @@
 const PersonForm = ({ onSubmit, newName, onNameChange, newNumber, onNumberChange }) => <form onSubmit={onSubmit}>
   <div>
-    name: <input value={newName} onChange={onNameChange} />
+    name: <input 
+      value={newName} 
+      onChange={onNameChange}
+      placeholder="at least 3 characters" />
   </div>
   <div>
-    number: <input value={newNumber} onChange={onNumberChange} />
+    number: <input 
+      value={newNumber} 
+      onChange={onNumberChange}
+      placeholder="e.g. 040-1234567" />
   </div>
   <div>
     <button type="submit">add</button>
