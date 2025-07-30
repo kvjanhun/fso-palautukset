233fba06f7af1a0fc29cb2998db51c6f9cdb1c0c
erezac <konsta@erez.ac>
2025-07-30 12:37:33 +0300

    Refactoring

--- osa4/bloglist/README.md
+++ osa4/bloglist/README.md
@@ -1,5 +1,8 @@
 # Blog List app
 Backend for a blogs listing and rating application.
+
 `npm install` to install dependencies
+
 `npm run dev` to run with `--watch`
+
 `npm run start` to start
--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,38 +1,19 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blog')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+logger.info(`Connected to MongoDB at ${mongoUrl}`)
 
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT || 3003
+const PORT = config.PORT || 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
