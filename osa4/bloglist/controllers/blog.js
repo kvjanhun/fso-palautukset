22898dc73ec74fb2d9bad3332c5bf4db2b7cb532
erezac <konsta@erez.ac>
2025-08-01 00:01:28 +0300

    4.10: Blog List Tests, step 3

--- osa4/bloglist/controllers/blog.js
+++ osa4/bloglist/controllers/blog.js
@@ -1,18 +1,21 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
+blogRouter.post('/', async (request, response) => {
+  const body = request.body
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes
   })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogRouter
---
+++ osa4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,92 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const assert = require('node:assert')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'A blog about Node.js',
+    author: 'A. Nerd',
+    url: 'https://erez.ac',
+    likes: 3
+  },
+  {
+    title: 'Blog about testing',
+    author: 'Test Author',
+    url: 'https://url.test/1',
+    likes: 0
+  },
+  {
+    title: 'Blog about blogging',
+    author: 'Test Author',
+    url: 'https://url.test/2',
+    likes: 1
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+
+  // const blogObjects = initialBlogs
+  //   .map(blog => new Blog(blog))
+  // const promiseArray = blogObjects.map(blog => blog.save())
+  // await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, 3)
+})
+
+test('blogs\' unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    // console.log(`Checking title '${blog.title}'`)
+    assert.ok(blog.id)
+    // console.log(` blog.id has value '${blog.id}'`)
+    assert.strictEqual(blog._id, undefined)
+    // console.log(` blog._id is ${blog._id}`)
+    // console.log('---')
+  })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Importance of regression testing',
+    author: 'A. Smartman',
+    url: 'https://www.com/important_blog.php',
+    likes: 69
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const newBlogExists = response.body.some(blog =>
+    blog.title === newBlog.title &&
+    blog.author === newBlog.author &&
+    blog.url === newBlog.url &&
+    blog.likes === newBlog.likes
+  )
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+  assert.strictEqual(newBlogExists, true)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
