68e53b11a41f305816892a61c1c7f3fc89098be9
erezac <konsta@erez.ac>
2025-08-05 18:10:12 +0300

    Add new describe block, Add test for finding a specific log within all returned

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('node:assert')
@@ -9,37 +9,45 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
+describe('When there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
 
-  // const blogObjects = initialBlogs
-  //   .map(blog => new Blog(blog))
-  // const promiseArray = blogObjects.map(blog => blog.save())
-  // await Promise.all(promiseArray)
-})
+    // const blogObjects = initialBlogs
+    //   .map(blog => new Blog(blog))
+    // const promiseArray = blogObjects.map(blog => blog.save())
+    // await Promise.all(promiseArray)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 3)
-})
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
 
-test('blogs\' unique identifier property is named id', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.forEach(blog => {
-    // console.log(`Checking title '${blog.title}'`)
-    assert.ok(blog.id)
-    // console.log(` blog.id has value '${blog.id}'`)
-    assert.strictEqual(blog._id, undefined)
-    // console.log(` blog._id is ${blog._id}`)
-    // console.log('---')
+  test('blogs\' unique identifier property is named id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const comparableBlog = helper.validBlog
+
+    const blogExists = response.body.some(blog =>
+      helper.blogsAreEqual(blog, comparableBlog)
+    )
+    assert.strictEqual(blogExists, true)
   })
 })
 
@@ -54,10 +62,7 @@ test('a valid blog can be added', async () => {
 
   const response = await api.get('/api/blogs')
   const newBlogExists = response.body.some(blog =>
-    blog.title === newBlog.title &&
-    blog.author === newBlog.author &&
-    blog.url === newBlog.url &&
-    blog.likes === newBlog.likes
+    helper.blogsAreEqual(blog, newBlog)
   )
   assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
   assert.strictEqual(newBlogExists, true)
@@ -99,20 +104,18 @@ test('blog\'s missing url returns 400', async () => {
 
 test('blog deletion succeeds with status code 204 if id valid', async () => {
   const blogsBefore = await api.get('/api/blogs')
-  // console.log('blogsBefore:', blogsBefore.body)
   const deletedBlog = blogsBefore.body[0]
-  // console.log('deletedBlog:', deletedBlog)
 
   await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204)
 
   const blogsAfter = await api.get('/api/blogs')
-  // console.log('blogsAfter:', blogsAfter.body)
+
   assert.strictEqual(blogsAfter.body.length, blogsBefore.body.length - 1)
 
   const deletedBlogExists = blogsAfter.body.some(blog =>
     helper.blogsAreEqual(blog, deletedBlog)
   )
-  // console.log('deletedBlogExists:', deletedBlogExists)
+
   assert.strictEqual(deletedBlogExists, false)
 })
 
