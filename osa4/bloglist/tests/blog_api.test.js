6cba8f0ccb5f14cd0bde3df650f9ddc38abf5968
erezac <konsta@erez.ac>
2025-08-05 18:39:22 +0300

    Refactor existing tests, Add test for deletion with invalid id

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -9,17 +9,12 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
 describe('When there are initially some blogs saved', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-
-    // const blogObjects = initialBlogs
-    //   .map(blog => new Blog(blog))
-    // const promiseArray = blogObjects.map(blog => blog.save())
-    // await Promise.all(promiseArray)
-  })
-
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -51,72 +46,84 @@ describe('When there are initially some blogs saved', () => {
   })
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = helper.validBlog
+describe('Adding a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = helper.validBlog
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/blogs')
-  const newBlogExists = response.body.some(blog =>
-    helper.blogsAreEqual(blog, newBlog)
-  )
-  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
-  assert.strictEqual(newBlogExists, true)
+    const response = await api.get('/api/blogs')
+    const newBlogExists = response.body.some(blog =>
+      helper.blogsAreEqual(blog, newBlog)
+    )
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(newBlogExists, true)
+  })
+
+  test('fails with status code 400 if title is missing', async () => {
+    const titlelessBlog = helper.titlelessBlog
+    // NOTE! Add check that nothing was added to database.
+    await api
+      .post('/api/blogs')
+      .send(titlelessBlog)
+      .expect(400)
+  })
+
+  test('fails with status code 400 if url is missing', async () => {
+    const urllessBlog = helper.urllessBlog
+    // NOTE! Add check that nothing was added to database.
+    await api
+      .post('/api/blogs')
+      .send(urllessBlog)
+      .expect(400)
+  })
+
+  test('succeeds with likes defaulting to 0 if missing', async () => {
+    const newBlog = helper.noLikesBlog
+    // console.log(newBlog)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs/')
+    const savedBlog = response.body.find(
+      blog => blog.title === newBlog.title)
+    // console.log(savedBlog)
+    assert.strictEqual(savedBlog.likes, 0)
+  })
 })
 
-test('blogs\' likes default to 0', async () => {
-  const newBlog = helper.noLikesBlog
-  // console.log(newBlog)
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+describe('Deleting a blog', () => {
+  test('succeeds with status code 204 if id valid', async () => {
+    const blogsBefore = await api.get('/api/blogs')
+    const deletedBlog = blogsBefore.body[0]
 
-  const response = await api.get('/api/blogs/')
-  const savedBlog = response.body.find(
-    blog => blog.title === newBlog.title)
-  // console.log(savedBlog)
-  assert.strictEqual(savedBlog.likes, 0)
-})
+    await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204)
 
-test('blog\'s missing title returns 400', async () => {
-  const titlelessBlog = helper.titlelessBlog
-  // NOTE! Add check that nothing was added to database.
-  await api
-    .post('/api/blogs')
-    .send(titlelessBlog)
-    .expect(400)
-})
+    const blogsAfter = await api.get('/api/blogs')
 
-test('blog\'s missing url returns 400', async () => {
-  const urllessBlog = helper.urllessBlog
-  // NOTE! Add check that nothing was added to database.
-  await api
-    .post('/api/blogs')
-    .send(urllessBlog)
-    .expect(400)
-})
+    assert.strictEqual(blogsAfter.body.length, blogsBefore.body.length - 1)
 
-test('blog deletion succeeds with status code 204 if id valid', async () => {
-  const blogsBefore = await api.get('/api/blogs')
-  const deletedBlog = blogsBefore.body[0]
+    const deletedBlogExists = blogsAfter.body.some(blog =>
+      helper.blogsAreEqual(blog, deletedBlog)
+    )
 
-  await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204)
+    assert.strictEqual(deletedBlogExists, false)
+  })
 
-  const blogsAfter = await api.get('/api/blogs')
+  test('fails with status code 400 if id is invalid', async () => {
+    const invalidId = '12345'
+    await api.delete(`/api/blogs/${invalidId}`).expect(400)
 
-  assert.strictEqual(blogsAfter.body.length, blogsBefore.body.length - 1)
-
-  const deletedBlogExists = blogsAfter.body.some(blog =>
-    helper.blogsAreEqual(blog, deletedBlog)
-  )
-
-  assert.strictEqual(deletedBlogExists, false)
+    const blogsAfter = await api.get('/api/blogs')
+    assert.strictEqual(blogsAfter.body.length, helper.initialBlogs.length)
+  })
 })
 
 after(async () => {
